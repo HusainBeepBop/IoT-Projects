7b87b9069261daa8cd9bbcf5125160dc6bff36a1
Husain <husainlokhandwala33@gmail.com>
2025-03-19 23:26:59 +0530

    pulldown button test

---
+++ ESP32_BUTTON_PULLDOWN_TEST/ESP32_BUTTON_PULLDOWN_TEST.ino
@@ -0,0 +1,17 @@
+const int buttonPin = 15;  // GPIO15 for the button
+const int ledPin = 2;      // Onboard LED on GPIO2
+
+void setup() {
+  pinMode(buttonPin, INPUT);  // Button as input
+  pinMode(ledPin, OUTPUT);    // LED as output
+}
+
+void loop() {
+  int buttonState = digitalRead(buttonPin); // Read button state
+
+  if (buttonState == HIGH) {
+    digitalWrite(ledPin, HIGH);  // Turn on LED if button is pressed
+  } else {
+    digitalWrite(ledPin, LOW);   // Turn off LED if button is not pressed
+  }
+}
