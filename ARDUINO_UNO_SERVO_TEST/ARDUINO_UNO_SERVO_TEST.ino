14bfdf8d01c8498afc315b77fe8aa0fa1699d7e4
Husain <husainlokhandwala33@gmail.com>
2025-03-25 14:18:43 +0530

    servo test code

--- ARDUINO_UNO_SERVO_TEST/ARDUINO_UNO_SERVO_TEST.ino
+++ ARDUINO_UNO_SERVO_TEST/ARDUINO_UNO_SERVO_TEST.ino
@@ -1,7 +1,7 @@
 #include <Servo.h>
 
-Servo myServo;  // Create servo object
-const int servoPin = 9; // Pin connected to the servo on Arduino Uno
+Servo myServo;  
+const int servoPin = 9; 
 
 void setup() {
   myServo.attach(servoPin);
@@ -9,7 +9,7 @@ void setup() {
 }
 
 void loop() {
-  // Sweep from 0 to 180 degrees
+  
   for (int pos = 0; pos <= 180; pos += 10) {
     myServo.write(pos);
     Serial.print("Angle: ");
@@ -17,7 +17,7 @@ void loop() {
     delay(50);
   }
 
-  // Sweep from 180 to 0 degrees
+  
   for (int pos = 180; pos >= 0; pos -= 10) {
     myServo.write(pos);
     Serial.print("Angle: ");
